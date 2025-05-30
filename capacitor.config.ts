f111023de2cd807e492cc7a33c3b22c39e9f2ba0
nhantran522000 <nhan.tranthienthanh@gmail.com>
2025-05-30 08:30:34 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-ci-cd',
+  webDir: 'www'
+};
+
+export default config;
